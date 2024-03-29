0cd4c063ddbaea5bdc50649f2e82e39ca4b9f553
Pavel <pavelpredator6@gmail.com>
2024-03-29 22:09:06 +0300

    Refactor email sending logic in resend route

--- src/app/api/resend/route.ts
+++ src/app/api/resend/route.ts
@@ -8,13 +8,17 @@ export async function POST(request: NextRequest, response: NextResponse) {
   const { name, phoneNumber, email, message } = await request.json()
 
   try {
-    const data = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: 'digichips.ru@digichips.ru',
-      to: process.env.SMTP_EMAIL,
+      to: `${process.env.SMTP_EMAIL}`,
       subject: 'Новое сообщение от клиента с сайта',
       react: EmailTemplate({ firstName: name, phoneNumber, email, message }),
       reply_to: email,
+      text: `Новое сообщение с сайта digichips.ru. Имя: ${name}\nТелефон: ${phoneNumber}\nEmail: ${email}\nСообщение: ${message}`,
     })
+    if (error) {
+      return Response.json({ error })
+    }
 
     return Response.json(data)
   } catch (error) {
